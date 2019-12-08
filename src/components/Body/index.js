25210c3dfdca55197cf2245a4aca219c423e541b
eduqg <eduardoqgomes@gmail.com>
2019-12-07 23:29:26 -0300

    Little change to Body component

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -5,9 +5,9 @@ export const Body = styled.View`
   flex: 1;
   background: #ffffff;
   height: ${Dimensions.get('window').height};
-  width: ${Dimensions.get('window').width - 60};
-  margin-bottom: 40px;
-  margin-top: -10px;
+  width: ${Dimensions.get('window').width - 40};
+  margin-bottom: 20px;
+  margin-top: -26px;
   box-shadow: 10px 5px 5px #000;
   border-radius: 8px;
   padding: 20px 0;
